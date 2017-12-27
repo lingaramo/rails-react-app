a03ab07f8d29b0449ab7cc87c7c05c8df4689482
Lucas Ingaramo <l.ingaramo@gmail.com>
2017-12-26 22:08:49 -0300

    Remove admin user

--- app/models/admin.rb
+++
@@ -1,6 +0,0 @@
-class Admin < ActiveRecord::Base
-  # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
-  include DeviseTokenAuth::Concerns::User
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 
-  mount_devise_token_auth_for 'Admin', at: 'admin_auth', skip: [:omniauth_callbacks]
-
   as :admin do
     # Define routes for Admin within this block.
   end
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  # root to: "home#index"
+  get "*path", to: "home#index"
 end
